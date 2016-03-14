c2981c3bd698a7813099a561ecb643421a72a04a
Brandon Cook <bcook@thoughtworks.com>
2016-03-14 11:45:33 -0400

    Construct train labels and train movies

--- script/input_data.py
+++ script/input_data.py
@@ -1,9 +1,10 @@
 import csv
 import time
 import tmdbsimple as tmdb
+import numpy as np
 
 tmdb.API_KEY = 'fbcbee8de7fe5d215b5f7a16969027d3'
-attributes = { 'budget' : 0 }
+attributes = {}
 
 def read_data():
     movie_ids = []
@@ -14,31 +15,50 @@ def read_data():
             movie_ids.append(row[2])
     return movie_ids
 
-def create_train(movie_ids, batch_size):
+def create_data_set(movie_ids, batch_size):
     now = time.time()
+    labels = np.zeros((batch_size, 11))
     for x in range(batch_size):
         movie = tmdb.Movies(movie_ids[x])
         response = movie.info(append_to_response='credits')
-        create_movie_vector(movie)
+        create_movie_vector(movie, x)
+        rating = int(round(movie.vote_average))
+        labels[x, rating] = 1
 
         time.sleep(0.25) # Rate Limit for Tmdb is 40 requests every 10 seconds
 
     print "Time elapsed: " + str(round(time.time() - now, 2)) + " secs"
+    return np.zeros((batch_size, 5000)), labels
 
-def create_movie_vector(movie):
+def create_movie_vector(movie, index):
     print movie.title
-    add_production_companies(movie.production_companies)
+    add_production_companies(movie.production_companies, index)
+    add_cast_and_crew(movie.credits, index)
 
-def add_production_companies(companies):
+def add_cast_and_crew(credits, index):
+    for cast in credits['cast']:
+        person_id = cast['id']
+        if person_id not in attributes:
+            attributes[person_id] = [index]
+        else:
+            attributes[person_id].append(index)
+    for crew in credits['crew']:
+        person_id = crew['id']
+        if person_id not in attributes:
+            attributes[person_id] = [index]
+        else:
+            attributes[person_id].append(index)
+
+def add_production_companies(companies, index):
     for company in companies:
-        company_id = company['id']
-        if company_id not in attributes:
-            attributes[company_id] = len(attributes)
-
+        company_name = company['name']
+        if company_name not in attributes:
+            attributes[company_name] = [index]
+        else:
+            attributes[company_name].append(index)
 
 movies = read_data()
-train_set = create_train(movies, 50)
-print len(attributes)
+train_movies, train_labels = create_data_set(movies, 100)
 # test_set = create_train(movies, 500)
 # validation_set = create_train(movies, 500)
 
